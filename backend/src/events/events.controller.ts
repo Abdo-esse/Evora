8422b23e8be19e53c8d87e3472fcf37acf72c3af
abdelilah2005 <abdelilahessemlali@gmail.com>
2026-02-03 21:38:13 +0100

    EV-46 add controller crud event

---
+++ backend/src/events/events.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Post, Body, UseGuards, Req, Put, Delete, Get, Param, Query } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDto, UpdateEventDto } from 'src/common/dtos/events.dto';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { Roles } from '../common/decorators/role.decorators';
+import { Role } from 'src/common/enums/role';
+import { EventStatus } from 'src/common/enums/eventStatus';
+import { RolesGuard } from '../common/guards/role.guard';
+
+@Controller('events')
+export class EventsController {
+    constructor(private readonly eventsService: EventsService) { }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.ADMIN_ORG])
+    @Post()
+    async create(@Body() createEventDto: CreateEventDto, @Req() req) {
+        return this.eventsService.createEvent(createEventDto, req.user.sub);
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.ADMIN_ORG])
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+        return this.eventsService.updateEvent(id, updateEventDto);
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.ADMIN_ORG])
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return this.eventsService.deleteEvent(id);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get(':id')
+    async get(@Param('id') id: string, @Req() req) {
+        return this.eventsService.getEvent(id, req.user.role);
+    }
+
+    
+}
