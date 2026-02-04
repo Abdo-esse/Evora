72038bf8e7b6e81edb8eec714b9d733d5d1bafc9
abdelilah2005 <abdelilahessemlali@gmail.com>
2026-02-04 10:35:31 +0100

    EV-17 Validation par Admin-org

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, UseGuards, Req, Get, Param, Query } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get, Param, Query, Put } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from '../common/dtos/reservations.dto';
 import { AuthGuard } from '../common/guards/auth.guard';
 import { Roles } from '../common/decorators/role.decorators';
 import { Role } from '../common/enums/role';
 import { RolesGuard } from '../common/guards/role.guard';
+import { ReservationStatus } from '../common/enums/reservationStatus';
 
 @Controller('reservations')
 export class ReservationsController {
@@ -15,6 +16,27 @@ export class ReservationsController {
     @Post()
     async create(@Body() createReservationDto: CreateReservationDto, @Req() req) {
         return this.reservationsService.createReservation(createReservationDto, req.user.sub);
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.PARTICIPANT, Role.ADMIN_ORG])
+    @Get(':id')
+    async get(@Param('id') id: string, @Req() req) {
+        return this.reservationsService.getReservation(id, req.user.role, req.user.sub);
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.PARTICIPANT, Role.ADMIN_ORG])
+    @Get()
+    async getAll(@Query('page') page: number, @Query('limit') limit: number, @Query('search') search: string, @Query('status') status: ReservationStatus, @Req() req) {
+        return this.reservationsService.getAllReservations(req.user.role, req.user.sub, page, limit, search, status);
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles([Role.ADMIN_ORG])
+    @Put(':id/confirm')
+    async confirm(@Param('id') id: string, @Req() req) {
+        return this.reservationsService.updateReservationStatus(id, ReservationStatus.CONFIRMED, req.user.sub, req.user.role);
     }
 
 }
